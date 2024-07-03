47a1c791704e185136f441782009e5378a2188e3
evanbms <139075269+evanbms@users.noreply.github.com>
2024-07-03 14:13:15 +0000

    Endpoint create finalizado

--- index.js
+++ index.js
@@ -5,4 +5,22 @@ app.get('/', function (req, res) {
     res.send("Hello World")
 })
 
+app.get('/oi', function (req, res) {
+    res.send("Olá, mundo!")
+})
+
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read All - [GET] /item
+app.get('/item', function (req, res) {
+    // Pegamos a lista e enviamos como resposta HTTP
+res.send(lista)
+})
+
+// Create - [POST] /item
+app.post('/item', function (req, res) {
+res.send('Create')
+})    
+
 app.listen(3000)
