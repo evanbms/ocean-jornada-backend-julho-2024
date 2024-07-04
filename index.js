07594bf8ee9d2807079c6dc5a0f2cad7096fa7a2
evanbms <139075269+evanbms@users.noreply.github.com>
2024-07-04 12:51:14 +0000

    Endpoint Update [PUT] /item/:id implementado

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send("Hello World")
+  res.send('Hello World')
 })
 
+// Desafio: criar endpoint /oi que exibe "Olá, mundo!"
 app.get('/oi', function (req, res) {
-    res.send("Olá, mundo!")
+  res.send('Olá, mundo!')
 })
 
 // Lista de Personagens
@@ -14,13 +15,23 @@ const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 // Read All - [GET] /item
 app.get('/item', function (req, res) {
-    // Pegamos a lista e enviamos como resposta HTTP
-res.send(lista)
+  // Pegamos a lista e enviamos como resposta HTTP
+  res.send(lista)
 })
+
+// Sinalizamos para o Express que vamos usar JSON no Body
+app.use(express.json())
 
 // Create - [POST] /item
 app.post('/item', function (req, res) {
-res.send('Create')
-})    
+  // Obtemos o nome enviado no Request Body
+  const item = req.body.nome
+  
+  // Inserimos o item no final da lista
+  lista.push(item)
+ 
+  // Enviamos uma mensagem de sucesso!
+  res.send('Item criado com sucesso!')
+})
 
 app.listen(3000)
